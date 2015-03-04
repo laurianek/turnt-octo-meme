263bc9faf7196cabf5638be99b605d8f18f43803
Lauriane Kayungu-N <lauriane.kayungu@gmail.com>
2015-03-04 16:15:04 +0000

    quick looping trough an with bash script

---
+++ lib/app/auto.sh
@@ -0,0 +1,19 @@
+node index.js 2 25 29 39 41
+node index.js 8 14 35 41 49
+node index.js 17	26	27	45	49
+node index.js 4	13	21	25	29
+node index.js 15	17	24	31	38
+node index.js 11	16	32	33	37
+node index.js 12	14	35	41	45
+node index.js 19	28	36	42	43
+node index.js 4	16	31	36	42
+node index.js 7	12	21	34	47
+node index.js 9	12	28	45	50
+node index.js 19	27	28	39	45
+node index.js 5	25	26	29	37
+node index.js 7	13	18	24	31
+node index.js 10	26	30	39	50
+node index.js 13	15	29	30	38
+node index.js 10	17	24	32	50
+# node index.js 
+
